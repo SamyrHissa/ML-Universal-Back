ac3fedaad88715138028a3321cb523d9c2a9685c
PCSAMYRCASA\samyr <samyr@schissa.com.br>
2022-03-28 20:20:02 -0300

    Instalado o Cors

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,13 @@
-import express from "express";
+import express from 'express'
+import cors from 'cors'
+
 import {AddressInfo} from "net";
 import { userRouter } from "./router/UserRouter";
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 app.use("/users", userRouter);
 
